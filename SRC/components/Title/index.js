33cd0f1d194bfb333158b353bf5624b8e4d8f257
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-04-15 12:06:23 -0300

    criar Main

--- SRC/components/Title/index.js
+++ SRC/components/Title/index.js
@@ -5,7 +5,7 @@ export default function Title(){
     return(
         <View>
             <Text>ONEBITHEALTH</Text>
-            <Text>01/04/2024 3B3 Lado A</Text>
+           
         </View>
     );
 }
